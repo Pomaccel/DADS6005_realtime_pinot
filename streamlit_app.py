0a61f3f2d4ce56976fd5df432aac9cd0067dc2df
Bunrawat Charoenyuennan <158060569+Pomaccel@users.noreply.github.com>
2024-11-13 17:45:05 +0000

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	modified:   streamlit_app.py #	new file:   your_database.db

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import time
 
 st.set_page_config(page_title="Gundam Views Dashboard", layout="wide")
 st.title("📊 DADS6005 Realtime Dashboard : Mobile Suit Gundam")
-st.header("Explore the data visualizations below to see insights on Mobile Suit Gundam and trends over time.")
+st.write("Explore the data visualizations below to see insights on Mobile Suit Gundam and trends over time.")
 
 # Connect to Pinot database
 conn = connect(host='54.255.188.63', port=8099, path='/query/sql', schema='http')
@@ -27,24 +27,32 @@ if "auto_refresh" not in st.session_state:
 auto_refresh = st.checkbox('Auto Refresh?', st.session_state.auto_refresh)
 st.session_state.auto_refresh = auto_refresh
 
-if auto_refresh:
-    refresh_rate = st.number_input('Refresh rate in seconds', value=st.session_state.sleep_time, min_value=1)
-    st.session_state.sleep_time = refresh_rate
-else:
-    refresh_rate = st.session_state.sleep_time
 
-# Create an Interative Filter 
-##  Create an Interative Filter fror Gundam Grade 
-curs = conn.cursor()
-curs.execute("SELECT DISTINCT GRADE FROM TP6_tumbling")
-grade = [row[0] for row in curs]
-selected_grade = st.multiselect("Select Gundam Grade:", grade, default= grade)
+## Create an Interative Filter 
 
-##  Create an Interative Filter fror Gender  
-curs.execute("SELECT DISTINCT GENDER FROM TP6_tumbling")
-genders = [row[0] for row in curs]
-selected_genders = st.multiselect("Select Genders:", genders, default=genders)
+col1, col2, col3 = st.columns(3)
 
+## Create an Interactive Filter for Gundam Grade 
+with col1:
+    curs = conn.cursor()
+    curs.execute("SELECT DISTINCT GRADE FROM TP6_tumbling")
+    grade = [row[0] for row in curs]
+    selected_grade = st.multiselect("Select Gundam Grade:", grade, default=grade)
+
+with col2:
+    curs = conn.cursor()
+    curs.execute("SELECT DISTINCT GENDER FROM TP6_tumbling")
+    genders = [row[0] for row in curs]
+    selected_genders = st.multiselect("Select Genders:", genders, default=genders)
+
+with col3:
+    if auto_refresh:
+        refresh_rate = st.number_input('Refresh rate in seconds', value=st.session_state.sleep_time, min_value=1)
+        st.session_state.sleep_time = refresh_rate
+    else:
+        refresh_rate = st.session_state.sleep_time
+
+# Generate filters for SQL query
 grade_filter = "'" + "', '".join(selected_grade) + "'"
 gender_filter = "'" + "', '".join(selected_genders) + "'"
 
@@ -56,23 +64,24 @@ SELECT
     COUNT(GENDER) AS visitor
 FROM 
     TP6_tumbling
+WHERE 
+    GRADE IN ({grade_filter}) AND GENDER IN ({gender_filter})
 GROUP BY 
     GENDER, 
     GUNDAM_NAME
 ORDER BY 
     visitor DESC
-LIMIT 500;
-
+LIMIT 1000;
 """
 curs.execute(query1)
 df1 = pd.DataFrame(curs, columns=[item[0] for item in curs.description])
 df1 = df1.sort_values(by="visitor", ascending=False)
 
 # Create the first column layout
-col1, col2 = st.columns(2)
+col3, col4 = st.columns(2)
 
 # Plot the first graph in the first column
-with col1:
+with col3:
     fig1 = px.bar(df1, 
                   x="visitor", 
                   y="GUNDAM_NAME", 
@@ -87,11 +96,11 @@ with col1:
         xaxis_title="Total Visits",             
         yaxis_title=None
     )
-    st.plotly_chart(fig1)
+    st.header("🤖 Gundam Views by Name")
+    st.plotly_chart(fig1, use_container_width=True)
 
 
 ###--------------------------------------------------------------
-
 # Query 2: 
 query2 = f"""
 SELECT
@@ -101,9 +110,11 @@ SELECT
     WINDOW_END_STR
 FROM
     TP7_hopping
+WHERE 
+    GRADE IN ({grade_filter}) AND GENDER IN ({gender_filter})
 GROUP BY
     GENDER, WINDOW_START_STR, WINDOW_END_STR
-LIMIT 20000 ;
+LIMIT 1000;
 """
 curs.execute(query2)
 df2 = pd.DataFrame(curs, columns=[item[0] for item in curs.description])
@@ -128,7 +139,7 @@ timeperiods_last_5 = unique_timeperiods[-5:]
 # Filter the result to include only the last 5 time periods
 result = result[result['TimePeriod'].isin(timeperiods_last_5)]
 
-with col2:
+with col4:
     # Plot using 'result' dataframe instead of 'df2'
     fig2 = px.bar(result,
                    x="TimePeriod", 
@@ -145,15 +156,14 @@ with col2:
         yaxis=dict(showgrid=False),
         barmode='stack'
     )
-    st.plotly_chart(fig2)
+    st.header("👨‍👨‍👧‍👧 Total Views by Gender Over Time")
+    st.plotly_chart(fig2, use_container_width=True)
 
 # Second row with two more columns
-col3, col4 = st.columns(2)
+col5, col6 = st.columns(2)
 
+##--------------------------------------
 
-
-
-# Query 3: Tracking page visits by category over time
 query3 = f"""
 SELECT
     GRADE,
@@ -161,10 +171,11 @@ SELECT
     COUNT(GENDER) AS VISITOR
 FROM
     TP7_hopping
+WHERE GRADE IN ({grade_filter}) AND GENDER IN ({gender_filter})
 GROUP BY
     GRADE, GENDER
 
-LIMIT 10000 ;
+LIMIT 5000;
 """
 curs.execute(query3)
 df_summary3 = pd.DataFrame(curs, columns=[item[0] for item in curs.description])
@@ -173,7 +184,7 @@ fig = px.bar(df_summary3, x='GRADE', y='VISITOR', color='GENDER', barmode='group
              labels={'VISITOR': 'Number of Visitors', 'GRADE': 'Grade Level', 'GENDER': 'Gender'},
              title="Visitors Grouped by Grade and Gender")
 
-with col3:
+with col5:
     fig3 = px.bar(df_summary3, 
                   x="GRADE", 
                   y="VISITOR", 
@@ -190,11 +201,10 @@ with col3:
         xaxis=dict(showgrid=False),       
         yaxis=dict(showgrid=False)                        
     )
-    st.plotly_chart(fig3)
+    st.header("⭐ Gundam Views by Grade")
+    st.plotly_chart(fig3, use_container_width=True)
 
-    
-
-# Query 4: Average visitor per session by gender
+##--------------------------------------
 query4 = f"""
 SELECT 
     GENDER, 
@@ -203,34 +213,40 @@ SELECT
     (AVG(SESSION_LENGTH_MS) / 60000) / COUNT(GENDER) AS SESSION_LENGTH_PER_VISITOR_MIN
 FROM 
     TP8_session
+WHERE GRADE IN ({grade_filter}) AND GENDER IN ({gender_filter})
 GROUP BY 
     GENDER
-LIMIT 1000000;
+LIMIT 100;
 """
 
 curs.execute(query4)
 df_summary4 = pd.DataFrame(curs, columns=[item[0] for item in curs.description])
 
 # Plotting with Plotly
-with col4:
+with col6:
     fig4 = px.bar(df_summary4,
                 x="GENDER",
                 y="AVG_SESSION_LENGTH_MIN",
                 title="Session Length vs Total Visitors by Gender",
                 labels={"TOTAL_VISITOR": "Total Visitors", "AVG_SESSION_LENGTH_MIN": "Average Session Length (min)"},
-                color='GENDER', 
+                color='GENDER',
                 hover_data=["TOTAL_VISITOR"])  # Add TOTAL_VISITOR to hover data
 
+    # Update traces to position text appropriately (e.g., 'inside' for text inside the bars)
     fig4.update_traces(textposition='inside')
-    fig4.update_layout(
-        plot_bgcolor='rgba(0, 0, 0, 0)', 
-        xaxis_title="Total Visitors",             
-        yaxis_title="Average Session Length (min)",  
-        xaxis=dict(showgrid=False),       
-        yaxis=dict(showgrid=False)                        
-    )
-    st.plotly_chart(fig4)
 
+    # Update layout for cleaner appearance
+    fig4.update_layout(
+        plot_bgcolor='rgba(0, 0, 0, 0)',
+        xaxis_title="Time Period",
+        yaxis_title=None,
+        xaxis=dict(showgrid=False),
+        yaxis=dict(showgrid=False)
+    )
+    st.header("⚧ Gender Type by Grade")
+    st.plotly_chart(fig4, use_container_width=True)
+
+#--------------------------------------------------------------------------------
 # Refresh logic
 if auto_refresh:
     time.sleep(refresh_rate)
